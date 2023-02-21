f8f34c4df301aee77251a4cc4cbd25a923419abb
tiendat3699 <onesteep00@gmail.com>
2023-02-21 17:28:58 +0700

    update flip skill

--- Assets/Scripts/Skills/AbsSkill.cs
+++ Assets/Scripts/Skills/AbsSkill.cs
@@ -1,8 +1,53 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+public enum skillHolder{
+    skill1,
+    skill2,
+    skill3,
+    skill4
+}
 
 public abstract class AbsSkill : MonoBehaviour
 {
-    public abstract void Active();
+    public string skillName;
+    public string skillDescription;
+    public skillHolder skillHolder;
+    [SerializeField] private float cooldownTime;
+    [SerializeField] private float executionTime;
+    private float cooldownTimer;
+    private bool ready = true;
+    public event Action<float> OnCoolDown;
+    public event Action OnDoneExecuting;
+
+    protected abstract void Action();
+    public void Cast()
+    {
+        if (ready)
+        {
+            ready = false;
+            Action();
+            StartCoroutine(CoolDownCoroutine());
+            Invoke("DoneExecution", executionTime);
+        }
+    }
+
+    private IEnumerator CoolDownCoroutine()
+    {
+        cooldownTimer = cooldownTime;
+        while (cooldownTimer >= 0)
+        {
+            cooldownTimer -= Time.deltaTime;
+            OnCoolDown?.Invoke(cooldownTimer);
+            yield return null;
+        }
+        cooldownTimer = cooldownTime;
+        ready = true;
+    }
+
+    private void DoneExecution()
+    {
+        OnDoneExecuting?.Invoke();
+    }
 }
